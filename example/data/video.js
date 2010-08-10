b59adc6826657e282cbb71a32dd93b1a21f797a7
walexy <bill@hxgp.net>
2010-08-09 23:46:59 -0500

    Added json data file to examples

---
+++ example/data/video.js
@@ -0,0 +1,82 @@
+[
+	{
+		"categoryLabel": "Cooking Techniques",
+		"sessions": [
+        			{
+        				"server":"stream.server.com/video",
+	        			"name": "Session 1, Part 1",
+	        			"file":"pathto/SomeVideo",			// no file extensions allows
+	        			"poster": "pathto/thePoster",		// for greater flexibility when building out the template
+	        			"description":"The best jello mold technique you'll find on the web"
+	        		},
+        			{
+	        			"server":"stream.server2.com/video",
+	        			"name": "Session 1, Part 2",
+	        			"file":"pathto/theVideo2",
+	        			"poster": "pathto/thePoster2",
+	        			"description":null
+	        		},
+        			{
+	        			"server":"stream.server.com/video",
+	        			"name": "Session 1, Part 3",
+	        			"file":"pathto/SomeVideo3",
+	        			"poster": "pathto/ThePoster3",
+	        			"description":null
+	        		},
+	        		{
+        				"server":"stream.server.com/video",
+	        			"name": "Session 1, Part 1",
+	        			"file":"pathto/SomeVideo",
+	        			"poster": "pathto/thePoster",
+	        			"description":"The best jello mold technique you'll find on the web"
+	        		},
+        			{
+	        			"server":"stream.server2.com/video",
+	        			"name": "Session 1, Part 2",
+	        			"file":"pathto/theVideo2",
+	        			"poster": "pathto/thePoster2",
+	        			"description":null
+	        		},
+        			{
+	        			"server":"stream.server.com/video",
+	        			"name": "Session 1, Part 3",
+	        			"file":"pathto/SomeVideo3",
+	        			"poster": "pathto/ThePoster3",
+	        			"description":null
+	        		},
+	        		{
+        				"server":"stream.server.com/video",
+	        			"name": "Session 1, Part 1",
+	        			"file":"pathto/SomeVideo",
+	        			"poster": "pathto/thePoster",
+	        			"description":"The best jello mold technique you'll find on the web"
+	        		},
+        			{
+	        			"server":"stream.server2.com/video",
+	        			"name": "Session 1, Part 2",
+	        			"file":"pathto/theVideo2",
+	        			"poster": "pathto/thePoster2",
+	        			"description":null
+	        		},
+        			{
+	        			"server":"stream.server.com/video",
+	        			"name": "Session 1, Part 3",
+	        			"file":"pathto/SomeVideo3",
+	        			"poster": "pathto/ThePoster3",
+	        			"description":null
+	        		}
+        ]
+	},
+	{
+		"categoryLabel": "Food Preparation",
+		"sessions": [
+        			{
+        				"server":"stream.server.com/video",
+	        			"name": "Julienne Carrots",
+	        			"file":"pathTo/theVideo4",
+	        			"poster": "pathto/TheCarrotVideoPoster",
+	        			"description":null
+	        		}
+        ]
+	}
+]
